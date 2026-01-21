336f56c4610a9e2a87b93383a1c2bb79773b65a5
sachinchetty27 <sachinchetty2707@gmail.com>
2026-01-21 12:13:29 +0530

    commit

---
+++ SimpleAddition/src/main/java/Exp1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Exp1 {
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter Number A and B");
+		int A= sc.nextInt();
+		int B=sc.nextInt();
+		//int A,B,C;
+		int C=A+B ;
+		System.out.println("total is : "+C);
+	}
+
+}
