48fec8d00685a6b4803189a6288d0d048ee38143
Mahesh Arun Aladi <mahesharunaladi@gmail.com>
2026-01-28 12:02:31 +0530

    Update Exp1.java
    latest update the variable

--- SimpleAddition/src/main/java/Exp1.java
+++ SimpleAddition/src/main/java/Exp1.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 public class Exp1 {
 	public static void main(String[] args) {
 		Scanner sc =new Scanner(System.in);
-		System.out.println("Enter Number A and B and D");// added by priyanshu
+		System.out.println("Enter Number K and B and D");// added by priyanshu
 		int A= sc.nextInt();
 		int B=sc.nextInt();
 				int D=sc.nextInt();// Added by Priyanshu
 
-		//int A,B,C;
-		int C=A+B ;
+		//int K,B,C;
+		int C=K+B ;
 		System.out.println("total is : "+C);
 	}
 
