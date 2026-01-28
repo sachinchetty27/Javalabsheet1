245b5c25ac37787cca577e4ff95f6906eb23a529
Priyanshu Singh <98865320+priyanshu5ingh@users.noreply.github.com>
2026-01-28 11:52:28 +0530

    Add prompt for three numbers in Exp1.java
    Added a new variable D to add 3 numbers

--- SimpleAddition/src/main/java/Exp1.java
+++ SimpleAddition/src/main/java/Exp1.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 public class Exp1 {
 	public static void main(String[] args) {
 		Scanner sc =new Scanner(System.in);
-		System.out.println("Enter Number A and B");
+		System.out.println("Enter Number A and B and D");// added by priyanshu
 		int A= sc.nextInt();
 		int B=sc.nextInt();
+				int D=sc.nextInt();// Added by Priyanshu
+
 		//int A,B,C;
 		int C=A+B ;
 		System.out.println("total is : "+C);
