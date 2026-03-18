d2e9f19f3e614ff76321bbf9ebe9c42355abfc6f
sachin <sachinchetty2707@gmail.com>
2026-03-18 12:01:27 +0530

    Update Exp1.java

--- SimpleAddition/src/main/java/Exp1.java
+++ SimpleAddition/src/main/java/Exp1.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
+
 public class Exp1 {
-	public static void main(String[] args) {
-		Scanner sc =new Scanner(System.in);
-		System.out.println("Enter Number K and B and D");// added by priyanshu
-		int A= sc.nextInt();
-		int B=sc.nextInt();
-				int D=sc.nextInt();// Added by Priyanshu
-
-		//int K,B,C;
-		int C=K+B ;
-		System.out.println("total is : "+C);
-	}
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter Number A and B:"); 
+        
+        // Read the inputs
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        
+        // You mentioned D, but if you just want to sum A and B:
+        int C = A + B; 
+        
+        System.out.println("total is : " + C);
+    }
 }
